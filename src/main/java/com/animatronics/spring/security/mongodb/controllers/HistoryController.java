78f0543413ac2b50741174d87e7e0cb1b26dcadc
Muhammad Azhar <117720953+MuhammadAzhar9@users.noreply.github.com>
2023-04-17 09:12:42 +0700

    History : Model

--- src/main/java/com/animatronics/spring/security/mongodb/controllers/HistoryController.java
+++ src/main/java/com/animatronics/spring/security/mongodb/controllers/HistoryController.java
@@ -1,5 +1,32 @@
 package com.animatronics.spring.security.mongodb.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.animatronics.spring.security.mongodb.models.Diary;
+import com.animatronics.spring.security.mongodb.models.User;
+import com.animatronics.spring.security.mongodb.models.Role;
+import com.animatronics.spring.security.mongodb.repository.HistoryRepository;
+import com.animatronics.spring.security.mongodb.repository.UserRepository;
+
+@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+@RequestMapping("/api")
 public class HistoryController {
-    
+
 }
--- src/main/java/com/animatronics/spring/security/mongodb/models/History.java
+++ src/main/java/com/animatronics/spring/security/mongodb/models/History.java
@@ -1,5 +1,67 @@
 package com.animatronics.spring.security.mongodb.models;
 
+import java.lang.annotation.Inherited;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "history")
 public class History {
-    
+    @Id
+    private String id;
+    private String desc;
+    private String balanceDesc;
+    private LocalDateTime time;
+
+    public History() {
+
+    }
+
+    public History(String id, String desc, String balanceDesc) {
+        this.id = id;
+        this.desc = desc;
+        this.balanceDesc = balanceDesc;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getBalanceDesc() {
+        return balanceDesc;
+    }
+
+    public void setBalanceDesc(String balanceDesc) {
+        this.balanceDesc = balanceDesc;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMMM yyyy");
+        String formattedDate = getTime().format(formatter);
+        return "History [id=" + id + ", desc=" + desc + ", balanceDesc=" + balanceDesc + ", time=" + formattedDate + "]";
+    }
+
 }
