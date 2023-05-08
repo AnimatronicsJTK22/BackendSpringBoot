4d6cbe67abe7a97987dec870b2c97ff15045b852
nezrayy <fanez.fthd@gmail.com>
2023-05-08 08:04:21 +0700

    try lcd

--- src/main/java/com/animatronics/spring/security/mongodb/controllers/MDController.java
+++ src/main/java/com/animatronics/spring/security/mongodb/controllers/MDController.java
@@ -1,14 +1,14 @@
 package com.animatronics.spring.security.mongodb.controllers;
 
 import java.util.List;
-
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import com.animatronics.spring.security.mongodb.payload.request.MoneyDisciplineRequest;
+//import com.animatronics.spring.security.mongodb.payload.request.MoneyDisciplineRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -39,20 +39,19 @@ public class MDController {
     UserRepository userRepository;
 
     @PutMapping("/money/{id}")
-    public ResponseEntity<MoneyDiscipline> updateMD(@PathVariable("id") String id,
-            @RequestBody MoneyDisciplineRequest request) {
+    public ResponseEntity<MoneyDiscipline> updateMD(@PathVariable("id") String id, @RequestBody Map<String, Object> request) {
         Optional<MoneyDiscipline> mdData = mdRepository.findById(id);
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String user = ((UserDetails) authentication.getPrincipal()).getUsername();
-
+    
         if (mdData.isPresent()) {
             MoneyDiscipline _md = mdData.get();
-            double deposit = request.getDeposit();
-            double withdraw = request.getWithdraw();
-            String lcd = request.getLastChangeDesc();
+            double deposit = Double.parseDouble(request.get("deposit").toString());
+            double withdraw = Double.parseDouble(request.get("withdraw").toString());
+            String lcd = (String) request.get("lastChangeDesc");
             double tempBalance = _md.getBalance() + deposit - withdraw;
             String mdOwner = _md.getOwner();
-
+    
             if (user.equals(mdOwner)) {
                 if (tempBalance < 0) {
                     return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
